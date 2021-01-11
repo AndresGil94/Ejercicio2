7a662eec4895e0724d74ce8aaebdb1d0d75bd2dc
Andrés <Andrs@DESKTOP-QRLIISQ>
2021-01-11 13:13:38 +0100

    Update

---
+++ Ejercicio2/src/entities/Cliente.java
@@ -0,0 +1,40 @@
+package entities;
+
+public class Cliente {
+
+	/*
+	 *Variables de la clase Cliente obtenido de los constructores de Cliente de la Clase "TestBanco" */
+	
+	private String dni;
+	private String nombre;
+	private String saldo;
+	
+	/*Constructor parametrizado en el orden que se nos muestra en el constructor de la Clase "TestBanco" */
+	public Cliente(String dni, String nombre, String saldo) {
+		
+		this.dni = dni;
+		this.nombre = nombre;
+		this.saldo = saldo;
+	}
+	/*Getters y Setters de las variables de la Clase Cliente */
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(String saldo) {
+		this.saldo = saldo;
+	}
+
+
+}
---
+++ Ejercicio2/src/entities/Cuenta.java
@@ -0,0 +1,41 @@
+package entities;
+
+public class Cuenta {
+
+	/*
+	 *Variables de la clase Cuenta obtenido de los constructores de Cuenta de la Clase "TestBanco",además de instanciar un objeto "cliente" */
+	private int numeroCuenta;
+	private int saldo;
+	private Cliente cliente;
+
+	/*Constructor parametrizado en el orden que se nos muestra en el constructor de la Clase "TestBanco" y en el que añadimos el objeto cliente
+	 * tal y como aparece en el constructor de cuenta de la Clase "TestBanco" */
+	public Cuenta(int numeroCuenta, int saldo, Cliente cliente) {
+		
+		this.numeroCuenta = numeroCuenta;
+		this.saldo = saldo;
+		this.cliente = cliente;
+	}
+	/*Getters y Setters de las variables de la Clase Cliente */
+	public int getNumeroCuenta() {
+		return numeroCuenta;
+	}
+	public void setNumeroCuenta(int numeroCuenta) {
+		this.numeroCuenta = numeroCuenta;
+	}
+	public int getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(int saldo) {
+		this.saldo = saldo;
+	}
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+	
+	
+}
